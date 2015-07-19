ac38271c8ac50fd3706464b57def638d68a04d7e
Fabio H. G. Barbosa <fabiohbarbosa@gmail.com>
2015-07-19 17:19:21 -0300

    Testes de aceitaocao

---
+++ src/test/java/br/com/devmedia/ci/html/IndexTestHTML.java
@@ -0,0 +1,19 @@
+package br.com.devmedia.ci.html;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+
+public class IndexTestHTML {
+
+    @Test
+    public void testGoogle() throws IOException {
+        WebClient webClient = new WebClient();
+        HtmlPage currentPage = webClient.getPage("http://www.google.com/");
+        assertEquals("Google", currentPage.getTitleText());
+    }
+}
